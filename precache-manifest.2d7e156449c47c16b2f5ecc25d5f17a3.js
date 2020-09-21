4d0e9446638971041ca37284c2d4124c3d2acc59
YevgenyBen <kain425@gmail.com>
2020-09-21 13:39:39 +0300

    deploy test

---
+++ precache-manifest.2d7e156449c47c16b2f5ecc25d5f17a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eccdaf5b8572717ce167240426e18e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3573365a82f2120045f6",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "170272eb2a23a35cd8b3",
+    "url": "/static/css/main.ae44e6f7.chunk.css"
+  },
+  {
+    "revision": "3573365a82f2120045f6",
+    "url": "/static/js/2.9e21bdb5.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/static/js/2.9e21bdb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "170272eb2a23a35cd8b3",
+    "url": "/static/js/main.e6b2205a.chunk.js"
+  },
+  {
+    "revision": "f294869ae1697dfe421d",
+    "url": "/static/js/runtime-main.7ecb0647.js"
+  }
+]);
