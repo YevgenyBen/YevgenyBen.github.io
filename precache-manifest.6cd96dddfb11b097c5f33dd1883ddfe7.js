1f0ee29320d39db95d13cd91da42af728ae731dd
YevgenyBen <kain425@gmail.com>
2020-10-10 15:18:26 +0300

    upload retry

---
+++ precache-manifest.6cd96dddfb11b097c5f33dd1883ddfe7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9918c3d85a2c704cda1537e57388b000",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1cb9c67d4211bfc7775",
+    "url": "/static/css/main.b51d17f3.chunk.css"
+  },
+  {
+    "revision": "7173aeb396380c0ea3fc",
+    "url": "/static/js/2.4229e075.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.4229e075.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1cb9c67d4211bfc7775",
+    "url": "/static/js/main.fd7ee9f9.chunk.js"
+  },
+  {
+    "revision": "e28afe49dd13b67ec417",
+    "url": "/static/js/runtime-main.ffbee768.js"
+  }
+]);
